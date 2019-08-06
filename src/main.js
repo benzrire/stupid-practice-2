6cd5bb502625a79bb4a4bb72906c1242d602ec5b
unknown <benzrire@outlook.com>
2019-08-06 14:13:18 +0700

    [Added] first commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import { faBars, faHome, faUser, faWrench } from '@fortawesome/free-solid-svg-icons'
+
+library.add([faBars, faHome, faUser, faWrench])
+
+Vue.component('fa-icon', FontAwesomeIcon)
+
 Vue.config.productionTip = false
 
 new Vue({
